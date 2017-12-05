795a633403ec3c68499830053fa77a0093cd4b12
Taisei Arakaki <e175746@ie.u-ryukyu.ac.jp>
2017-12-05 10:41:35 +0900

    ふぁーすとこみっと

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import jp.ac.uryukyu.ie.ex3.pair11.*;
+public class Main{
+	public static void main(String[] args){
+		Dice dice = new Dice();
+		dice.play();
+		System.out.println(dice.getVal());
+	}
+}
